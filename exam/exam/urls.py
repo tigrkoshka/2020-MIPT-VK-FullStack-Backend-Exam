6dc590a8be614a84350283d2ee9e1e30a10c487f
Tigran <tigrankoshkelyan@gmail.com>
2020-04-22 23:19:17 +0500

    Fixed group model and views

--- exam/exam/urls.py
+++ exam/exam/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from groups import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('create_group/', views.create_group),
+    path('join_group/', views.join_group),
+    path('leave_group/', views.leave_group),
+    path('group_list/', views.group_list),
 ]
--- exam/groups/models.py
+++ exam/groups/models.py
@@ -1,20 +1,19 @@
 from django.db import models
-
 from users.models import User
 
 
 class Group(models.Model):
-	name = models.CharField(blank=True, verbose_name="Название группы")
-	
-	class Meta:
-		verbose_name = 'Группа'
-		verbose_name_plural = 'Группы'
-	
+    name = models.CharField(blank=True, verbose_name="Название группы", max_length=50)
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
 
 class UserGroup(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Студент')
-	group = models.ForeignKey(Group, on_delete=models.CASCADE, verbose_name='Группа')
-	
-	class Meta:
-		verbose_name = 'Член группы'
-		verbose_name_plural = 'Члены группы'
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Студент')
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, verbose_name='Группа')
+
+    class Meta:
+        verbose_name = 'Член группы'
+        verbose_name_plural = 'Члены группы'
